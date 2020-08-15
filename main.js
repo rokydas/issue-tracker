da2f09cea858ae2ddd426926c4bdfde6575ba5f1
rokydas <droky136@gmail.com>
2020-08-16 02:10:24 +0600

    bug fixed and total, open, closed message print

--- main.js
+++ main.js
@@ -23,34 +23,54 @@ function submitIssue(e) {
 
 const closeIssue = id => {
   const issues = JSON.parse(localStorage.getItem('issues'));
-  const currentIssue = issues.find(issue => issue.id === id);
+  const currentIssue = issues.find(issue => issue.id == id);
+  // console.log(issues);
   currentIssue.status = 'Closed';
   localStorage.setItem('issues', JSON.stringify(issues));
   fetchIssues();
 }
 
 const deleteIssue = id => {
+  // console.log('delete', id);
   const issues = JSON.parse(localStorage.getItem('issues'));
-  const remainingIssues = issues.filter( issue.id !== id )
+  const remainingIssues = issues.filter(issue => issue.id != id );
+  console.log(remainingIssues);
   localStorage.setItem('issues', JSON.stringify(remainingIssues));
+  fetchIssues();
 }
 
 const fetchIssues = () => {
   const issues = JSON.parse(localStorage.getItem('issues'));
   const issuesList = document.getElementById('issuesList');
   issuesList.innerHTML = '';
-
+  let openIssues = 0;
+  let closeIssues = 0;
   for (var i = 0; i < issues.length; i++) {
     const {id, description, severity, assignedTo, status} = issues[i];
-
+    
     issuesList.innerHTML +=   `<div class="well">
                               <h6>Issue ID: ${id} </h6>
-                              <p><span class="label label-info"> ${status} </span></p>
-                              <h3> ${description} </h3>
-                              <p><span class="glyphicon glyphicon-time"></span> ${severity}</p>
+                              <p><span class="label label-info"> ${status} </span></p>`;
+    if(status == 'Open'){
+      issuesList.innerHTML += `<h3> ${description} </h3>`;
+      openIssues++;
+    }
+    else{
+      issuesList.innerHTML += `<h3><strike> ${description} </strike></h3>`;
+      closeIssues++;
+    }
+    issuesList.innerHTML +=   `<p><span class="glyphicon glyphicon-time"></span> ${severity}</p>
                               <p><span class="glyphicon glyphicon-user"></span> ${assignedTo}</p>
-                              <a href="#" onclick="setStatusClosed(${id})" class="btn btn-warning">Close</a>
+                              <a href="#" onclick="closeIssue(${id})" class="btn btn-warning">Close</a>
                               <a href="#" onclick="deleteIssue(${id})" class="btn btn-danger">Delete</a>
                               </div>`;
   }
+  // console.log(openIssues, closeIssues);
+  activeIssues(openIssues, closeIssues);
+}
+
+function activeIssues(openIssues, closeIssues){
+  document.getElementById('total-issues').innerHTML = `Total Issues: ${openIssues + closeIssues}` ;
+  document.getElementById('open-issues').innerHTML = `Open Issues: ${openIssues}`;
+  document.getElementById('closed-issues').innerHTML = `Closed Issues: ${closeIssues}`;
 }
